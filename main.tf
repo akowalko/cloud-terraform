7e9fd94c621ba250d4e2a9e05e5cffde083ee81f
akowalko <01178657@pw.edu.pl>
2025-06-05 22:17:14 +0200

    commit

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
