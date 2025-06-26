d625ad1354efd8e9aa0e4d5d4d6acc478f81bae3
akowalko <01178657@pw.edu.pl>
2025-06-26 23:31:53 +0200

    1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name          = "test-subnetwork"
+  name          = "test-subnet"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
