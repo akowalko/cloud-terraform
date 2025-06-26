bdba1af155ef598e192b5a6368ef1d89f0cbb7bf
akowalko <01178657@pw.edu.pl>
2025-06-26 23:35:42 +0200

    1

--- gke_cluster.tf
+++ gke_cluster.tf
@@ -17,7 +17,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = "e2-medium"
-    disk_size_gb = 80
+    disk_size_gb = 20
     disk_type    = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
