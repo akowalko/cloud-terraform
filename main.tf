2727560bc79444dbc6adcb116e194e2c4b573481
akowalko <01178657@pw.edu.pl>
2025-06-26 23:03:01 +0200

    compute instances

--- main.tf
+++ main.tf
@@ -12,3 +12,25 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
     ip_cidr_range = "192.168.10.0/24"
   }
 }
+
+resource "google_compute_instance" "k8s_node" {
+  count        = 4
+  name         = "k8s-node-${count.index + 1}"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 20
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
+    access_config {} # Enables external IP
+  }
+
+  tags = ["k8s-node"]
+}
