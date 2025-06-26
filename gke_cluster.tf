d954efebc2d08acada80c21ceb0720ae84d012b2
akowalko <01178657@pw.edu.pl>
2025-06-26 23:34:31 +0200

    1

--- gke_cluster.tf
+++ gke_cluster.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "primary" {
   name               = "gke-cluster"
   location           = var.region
   remove_default_node_pool = true
-  initial_node_count = 1  # wymagane, ale usuwamy default pool
+  initial_node_count       = 1
 
   network    = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.subnetwork.name
@@ -13,7 +13,7 @@ resource "google_container_node_pool" "primary_nodes" {
   cluster    = google_container_cluster.primary.name
   location   = var.region
 
-  node_count = 3
+  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
