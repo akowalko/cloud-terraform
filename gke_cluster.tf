51a1e4b3c2627625e5d6b2256c6f2e456d7d4bf4
akowalko <01178657@pw.edu.pl>
2025-06-26 23:30:30 +0200

    1

--- gke_cluster.tf
+++ gke_cluster.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
-
 resource "google_container_cluster" "primary" {
   name               = "gke-cluster"
   location           = var.region
