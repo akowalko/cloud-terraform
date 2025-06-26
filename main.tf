a05910728a2f74bf4ba22ba86adf48732d2f04ff
akowalko <01178657@pw.edu.pl>
2025-06-26 23:09:15 +0200

    12

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance" "k8s_node" {
 
   network_interface {
     network    = google_compute_network.vpc_network.id
-    subnetwork = google_compute_subnetwork.test-subnetwork.id
+    subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.id
     access_config {} # Enables external IP
   }
 
